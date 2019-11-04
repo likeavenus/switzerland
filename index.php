4f6efd2a2f61d774b4757474d006a763eecb5afb
Rafael <rafael_guliev94@list.ru>
2019-11-04 20:39:40 +0300

    add fixes

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ $name = $_POST['name'];
 $page = $_POST['page'];
 
 $to = 'rafael_guliev94@list.ru';
+//$to = 'scib.ru@myswitzerland.com';
 $subject = 'Подписка от клиента';
 $message = "<p>Клиент: $inputMail</p> <p>Имя: $name</p>  <p>Страница: $page</p> ";
 $headers = "Content-type:text/html; charset = utf-8 \r\n";
 $headers .= "From: exampleour@gmail.com";
 
 mail($to, $subject, $message, $headers);
+
+if (mail($to, $subject, $message, $headers)) {
+    echo true;
+} else {
+    echo false;
+}
 
 ?>
